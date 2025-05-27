bfd54a1309bfc80092020fb9814732b9207c724e
LeapgenSolution <websites@leapgen.ai>
2025-05-27 16:53:05 -0500

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+
+createRoot(document.getElementById('root')!).render(
+    <App />
+)
